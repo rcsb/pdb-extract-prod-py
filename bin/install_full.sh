fba136231ce73c58d322e16a8ef1b3946e108317
Vladimir Guranovic <vladimir@rcsb.rutgers.edu>
2022-10-29 07:16:48 -0400

    Adding more missing pieces needed for execution.

--- bin/install_full.sh
+++ bin/install_full.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+python3 -m pip install six
+
+runUser=wwwdev
+
 scrDir=`dirname ${0}`
 scrAbsDir="`( cd \"${scrDir}\" && pwd )`"
 
-. ${scrAbsDir}/setup.sh
-
-${scrAbsDir}/install.sh
+runuser -l ${runUser} -c ". ${scrAbsDir}/setup.sh; ${scrAbsDir}/install.sh"
 
