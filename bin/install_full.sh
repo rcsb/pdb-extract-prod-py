c48b388597a88aea31f1206840525551ed8b7b49
Vladimir Guranovic <vladimir@rcsb.rutgers.edu>
2023-03-13 10:14:28 -0400

    Adding more missing pieces needed for execution.

--- bin/install_full.sh
+++ bin/install_full.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+python3 -m pip install --upgrade pip
+
 python3 -m pip install six
+
+python3 -m pip install scikit-build
+python3 -m pip install cmake
+python3 -m pip install mmcif
+
 
 runUser=wwwdev
 
