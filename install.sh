347add384aff7d4bf54cb20c0101aa05914b0588
shaochenghua <chenghua.shao@rcsb.org>
2024-02-19 19:26:56 -0500

    v4.2 update 1

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+## For Bourne shell users:
+## Must run "source setup.sh" before executing this script to set up enviroment variables.
+
+function highlight_text {
+    echo -ne "\e[93m$1\e[0m"
+}
+
+function download_file {
+    local url=$1
+    local filename="${url##*/}"
+
+    if [[ ! -f "$filename" ]]; then
+        curl $url -O
+    else
+        echo "[*] file $(highlight_text $filename) already exists, skipping download"
+    fi
+}
+
+if [ ! -d packages ]
+then
+echo "[*] Make packages folder"
+mkdir packages
+else
+echo "[*] Packages folder exists"
+fi
+echo "[*] Change directory to Maxit installation folder"
+cd packages
+
+maxit_name="maxit-v11.100-prod-src"
+maxit_filename=${maxit_name}.tar.gz
+url_maxit=https://sw-tools.rcsb.org/apps/MAXIT/$maxit_filename
+echo "[*] Downloading Maxit suite from $(highlight_text $url_maxit)"
+download_file $url_maxit
+echo "[*] File $(highlight_text $maxit_filename) downloaded"
+echo "[*] Decompressing $(highlight_text $maxit_filename) starts"
+tar -zxvf $maxit_filename
+echo "[*] Decompressing $(highlight_text $maxit_filename) finished"
+echo "[*] Maxit installation starts"
+cd $maxit_name
+make binary
+
+cd ../..
+echo "[*] Maxit installation finished"
