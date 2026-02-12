dcdcc6cb0e681cc9dc1976fd2a7e0dc66366a2c0
Vladimir Guranovic <vladimir@rcsb.rutgers.edu>
2026-02-12 16:42:58 -0500

    Explicitly setting installation of mmcif version 0.92.0, since installation of the latest version is broken.

--- install_full.sh
+++ install_full.sh
@@ -4,7 +4,7 @@ python3 -m pip install --upgrade pip
 # python3 -m pip install six
 # python3 -m pip install scikit-build
 python3 -m pip install cmake
-python3 -m pip install mmcif
+python3 -m pip install mmcif==0.92.0
 
 
 runUser=wwwdev
