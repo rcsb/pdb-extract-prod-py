a95cce430b240e1ef873b455ace7e839d4257136
root <root@pdb-vm-linux-013.rcsb.rutgers.edu>
2026-06-17 13:31:50 -0400

    Fixing installation script by properly setting up two environments. This needs to be done until PdbExtract backend is modified to only use local user's environment.

--- install_full.sh
+++ install_full.sh
@@ -1,22 +1,37 @@
 #!/usr/bin/env bash
 
-pythonVersion=3.12
+# A portion of the whole PdbExtract system is relying on system python3
+# environment. That is why this environment needs to be set up here:
 
-webBaseDir=/data/wwpdb/pdb_extract_service/pdb-extract-web
+python3 -m pip install --upgrade pip
 
-source ${webBaseDir}/venv/bin/activate
+# python3 -m pip install six
+# python3 -m pip install scikit-build
+python3 -m pip install cmake
+python3 -m pip install mmcif==0.92.0
 
-pythonExe=python${pythonVersion}
 
-${pythonExe} -m pip install --upgrade pip
-
-# ${pythonExe} -m pip install six
-# ${pythonExe} -m pip install scikit-build
-${pythonExe} -m pip install cmake
-${pythonExe} -m pip install mmcif==0.92.0
-
+# A portions of the whole PdbExtract system is relying on custom python3
+# environment. That is why this environment needs to be set up here:
 
 runUser=wwwdev
+
+pythonVersion=3.12
+webBaseDir=/data/wwpdb/pdb_extract_service/pdb-extract-web
+
+sudo -u ${runUser} bash -c "
+    source ${webBaseDir}/venv/bin/activate
+
+    pythonExe=python${pythonVersion}
+
+    \${pythonExe} -m pip install --upgrade pip
+
+    # \${pythonExe} -m pip install six
+    # \${pythonExe} -m pip install scikit-build
+    \${pythonExe} -m pip install cmake
+    \${pythonExe} -m pip install mmcif
+"
+
 
 scrDir=`dirname ${0}`
 scrAbsDir="`( cd \"${scrDir}\" && pwd )`"
